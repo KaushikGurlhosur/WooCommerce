75ea58b99d13b17298e28021704f195fcd3598f3
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2025-10-15 20:51:21 +0530

    cors origin has been set

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ import cors from "cors";
 const app = express();
 
 // CORS setup
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: "https://woo-commerce-frontend-drab.vercel.app/" }));
 
 dotenv.config();
 
